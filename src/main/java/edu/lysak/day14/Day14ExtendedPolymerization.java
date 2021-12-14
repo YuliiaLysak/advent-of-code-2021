98b554e5e905529dd9947a371b4f5e0abfd30194
Yuliia Lysak <julia.m.lysak@gmail.com>
2021-12-14 22:00:45 +0200

    Day 14, part 2

--- src/main/java/edu/lysak/day14/Day14ExtendedPolymerization.java
+++ src/main/java/edu/lysak/day14/Day14ExtendedPolymerization.java
@@ -4,26 +4,25 @@ import edu.lysak.Utils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Day14ExtendedPolymerization {
     private static final String FILE_PATH = "src/main/resources/day14/input-day14.txt";
     private static final int STEP_COUNT = 10;
+    private static final int STEP_COUNT_2 = 40;
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Most common subtract least common = " + getPart1Result(FILE_PATH));
+        System.out.println("Most common subtract least common = " + getPart1ResultNotOptimal(FILE_PATH, STEP_COUNT));
+        System.out.println("Most common subtract least common (optimal) = " + getPart2ResultOptimal(FILE_PATH, STEP_COUNT_2));
     }
 
-    public static long getPart1Result(String filePath) throws IOException {
+    public static long getPart1ResultNotOptimal(String filePath, int stepCount) throws IOException {
         List<String> input = Utils.getInputDataAsStringList(filePath);
         Map<String, String> pairs = convertInputToInsertionPair(input);
         String line = input.get(0);
-        for (int i = 0; i < STEP_COUNT; i++) {
+        for (int i = 0; i < stepCount; i++) {
             line = insertChar(line, pairs);
         }
 
@@ -66,6 +65,97 @@ public class Day14ExtendedPolymerization {
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
+                .min(Map.Entry.comparingByValue())
+                .map(Map.Entry::getValue)
+                .orElseThrow();
+    }
+
+    public static long getPart2ResultOptimal(String filePath, int stepCount) throws IOException {
+        List<String> input = Utils.getInputDataAsStringList(filePath);
+        Map<String, String> insertionPairs = convertInputToInsertionPair(input);
+
+        String inputLine = input.get(0);
+        Map<String, Long> original = convertLineToPairMap(inputLine);
+        Map<String, Long> resultPairs = new HashMap<>(Map.copyOf(original));
+        for (int i = 0; i < stepCount; i++) {
+            resultPairs = insertChar(resultPairs, insertionPairs);
+        }
+
+        long mostCount = getMostCommonElementCount(
+                convertToCharPairs(resultPairs),
+                inputLine.charAt(0),
+                inputLine.charAt(inputLine.length() - 1)
+        );
+        long leastCount = getLeastCommonElementCount(
+                convertToCharPairs(resultPairs),
+                inputLine.charAt(0),
+                inputLine.charAt(inputLine.length() - 1)
+        );
+        return (mostCount - leastCount) / 2;
+    }
+
+    private static Map<String, Long> convertLineToPairMap(String line) {
+        List<String> pairs = new ArrayList<>();
+        for (int i = 0; i < line.length() - 1; i++) {
+            pairs.add(line.substring(i, i + 2));
+        }
+        return pairs.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    private static Map<String, Long> insertChar(Map<String, Long> input, Map<String, String> insertionPairs) {
+        Map<String, Long> output = new HashMap<>();
+        for (String pair : input.keySet()) {
+            if (insertionPairs.containsKey(pair)) {
+                Long count = input.getOrDefault(pair, 0L);
+                String newPair1 = pair.charAt(0) + insertionPairs.get(pair);
+                String newPair2 = insertionPairs.get(pair) + pair.charAt(1);
+                Long count1 = output.getOrDefault(newPair1, 0L);
+                output.put(newPair1, count1 + count);
+                Long count2 = output.getOrDefault(newPair2, 0L);
+                output.put(newPair2, count2 + count);
+            }
+        }
+        return output;
+    }
+
+    private static Map<Character, Long> convertToCharPairs(Map<String, Long> array) {
+        return array.entrySet().stream()
+                .map(pair -> {
+                    Map<Character, Long> charPairs = new HashMap<>();
+                    charPairs.merge(pair.getKey().charAt(0), pair.getValue(), Long::sum);
+                    charPairs.merge(pair.getKey().charAt(1), pair.getValue(), Long::sum);
+                    return charPairs;
+                })
+                .flatMap(charPairs -> charPairs.entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, Long::sum));
+    }
+
+    private static long getMostCommonElementCount(Map<Character, Long> chars, char startChar, char endChar) {
+        return chars.entrySet().stream()
+                .peek(pair -> {
+                    if (pair.getKey() == startChar) {
+                        pair.setValue(pair.getValue() + 1);
+                    }
+                    if (pair.getKey() == endChar) {
+                        pair.setValue(pair.getValue() + 1);
+                    }
+                })
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getValue)
+                .orElseThrow();
+    }
+
+    private static long getLeastCommonElementCount(Map<Character, Long> chars, char startChar, char endChar) {
+        return chars.entrySet().stream()
+                .peek(pair -> {
+                    if (pair.getKey() == startChar) {
+                        pair.setValue(pair.getValue() + 1);
+                    }
+                    if (pair.getKey() == endChar) {
+                        pair.setValue(pair.getValue() + 1);
+                    }
+                })
                 .min(Map.Entry.comparingByValue())
                 .map(Map.Entry::getValue)
                 .orElseThrow();
--- src/test/java/edu/lysak/day14/Day14ExtendedPolymerizationTest.java
+++ src/test/java/edu/lysak/day14/Day14ExtendedPolymerizationTest.java
@@ -9,12 +9,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class Day14ExtendedPolymerizationTest {
     private static final String FILE_PATH_1 = "src/test/resources/day14/input-day14-1.txt";
     private static final String FILE_PATH_2 = "src/test/resources/day14/input-day14-2.txt";
+    private static final int STEP_COUNT = 10;
+    private static final int STEP_COUNT_2 = 40;
 
     @Test
     void getPart1Result() throws IOException {
-        long result1 = Day14ExtendedPolymerization.getPart1Result(FILE_PATH_1);
-        long result2 = Day14ExtendedPolymerization.getPart1Result(FILE_PATH_2);
+        long result1 = Day14ExtendedPolymerization.getPart1ResultNotOptimal(FILE_PATH_1, STEP_COUNT);
+        long result2 = Day14ExtendedPolymerization.getPart1ResultNotOptimal(FILE_PATH_2, STEP_COUNT);
         assertEquals(1588, result1);
         assertEquals(2891, result2);
+    }
+
+    @Test
+    void getPart2Result() throws IOException {
+        long result1 = Day14ExtendedPolymerization.getPart2ResultOptimal(FILE_PATH_1, STEP_COUNT_2);
+        long result2 = Day14ExtendedPolymerization.getPart2ResultOptimal(FILE_PATH_2, STEP_COUNT_2);
+        assertEquals(2188189693529L, result1);
+        assertEquals(4607749009683L, result2);
     }
 }
